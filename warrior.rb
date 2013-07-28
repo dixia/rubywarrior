a9fae3e4dbdc5a0f70436668d78efaa95c1f6b44
dixia <iamh4n@gmail.com>
2013-07-28 19:38:22 +0200

    level 8

--- warrior.rb
+++ warrior.rb
@@ -1,36 +1,36 @@
 #https://www.bloc.io/ruby-warrior/#/warriors/10993/levels/7
 class Player
   
-  def initialize
-    super
-    @explored_back = false
-    @direction = :forward
-  end
-  
-  #def think_act(warrior,space,direction)
   def think_act(warrior,space)
-    if space.empty?
-      if warrior.health < 20
-        current_health = warrior.health
-        if current_health >= @health
-          warrior.rest!
-        else
-          if warrior.health < 15
-            warrior.walk!(:backward)
+    spaces = warrior.look
+    if not spaces.any? {|s| s.captive?} and spaces.any? {|s| s.enemy?}
+        warrior.shoot!  
+    else
+      if space.empty?
+        if warrior.health < 20
+          if warrior.health >= @health
+            #not in enemy's attack distance
+            warrior.rest!
           else
-            warrior.walk!
+            if warrior.health < 15
+              #defensive retreat
+              warrior.walk!(:backward)
+            else
+              #in a flight go straigt anyway
+              warrior.walk!
+            end
           end
+        else
+          warrior.walk!
         end
       else
-        warrior.walk! 
+          if space.enemy?
+              warrior.attack! 
+          end
+          if space.captive?
+              warrior.rescue! 
+          end
       end
-    else
-        if space.enemy?
-            warrior.attack!
-        end
-        if space.captive?
-            warrior.rescue!
-        end
     end
   end
   
@@ -41,7 +41,6 @@ class Player
     else
       think_act warrior,space
     end
-    
     @health = warrior.health
   end
 end
