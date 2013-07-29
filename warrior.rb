a28dc34a79c3031aea2b34103fcaf9b1939b504e
dixia <iamh4n@gmail.com>
2013-07-30 01:57:58 +0200

    Passed level 9 with some lucky.

--- warrior.rb
+++ warrior.rb
@@ -1,40 +1,96 @@
-#https://www.bloc.io/ruby-warrior/#/warriors/10993/levels/7
+#https://www.bloc.io/ruby-warrior/#/warriors/10993/levels/9
+
+#ugly code
 class Player
+  def initialize
+    super
+  end
   
-  def think_act(warrior,space)
-    spaces = warrior.look
-    if not spaces.any? {|s| s.captive?} and spaces.any? {|s| s.enemy?}
-        warrior.shoot!  
+  def is_not_being_attacked? warrior
+    warrior.health >= @health
+  end
+  
+  def is_being_attacked! warrior
+    if is_not_being_attacked? warrior
+      warrior.rest!
     else
+      if warrior.health < 15
+          #defensive retrea
+          #TODO detect attack direction
+          warrior.walk!(:backward)
+      else
+          #in a flight attack anyway
+          warrior.walk!
+      end
+    end
+  end
+  
+  def may_retract warrior
+    if warrior.health < 15
+      spaces_back = warrior.look(:backward)
+      spaces_front = warrior.look(:forward)
+      
+      if spaces_back.any? {|s| s.enemy?}
+        warrior.shoot!(:backward)
+      elsif spaces_front.any? {|s| s.enemy?}
+        warrior.shoot!(:forward)
+      else
+        warrior.walk!(:backward)
+      end
+      
+    else
+      warrior.rest!
+    end
+  end
+  
+  def may_use_sword space,warrior
       if space.empty?
         if warrior.health < 20
-          if warrior.health >= @health
-            #not in enemy's attack distance
-            warrior.rest!
-          else
-            if warrior.health < 15
-              #defensive retreat
-              warrior.walk!(:backward)
-            else
-              #in a flight go straigt anyway
-              warrior.walk!
-            end
-          end
+          is_being_attacked! warrior
         else
           warrior.walk!
         end
       else
           if space.enemy?
-              warrior.attack! 
+              warrior.attack!
+              @health = warrior.health
+              return true
           end
           if space.captive?
-              warrior.rescue! 
+              warrior.rescue!
+              @health = warrior.health
+              return true
           end
       end
+  end
+  
+  def think_act(warrior,space)
+    spaces = warrior.look
+    if not spaces.any? {|s| s.captive?} and spaces.any? {|s| s.enemy?} and space.empty?
+        unless is_not_being_attacked? warrior
+          unless may_use_sword space,warrior
+            warrior.shoot!
+            @health = warrior.health
+          end
+        else
+          if warrior.health < 20
+            may_retract warrior
+            warrior
+          else
+            warrior.walk!
+            @health = warrior.health
+          end
+        end
+    else
+      may_use_sword space,warrior
     end
   end
   
   def play_turn(warrior)
+    if @health.nil?
+      @health = warrior.health
+    end
+    
     space = warrior.feel
     if space.wall?
       warrior.pivot!
