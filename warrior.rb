61ba8e851459c3ce2f2117157f095ff00f9f89ed
dixia <iamh4n@gmail.com>
2013-08-01 22:50:06 +0200

    Better code

--- warrior.rb
+++ warrior.rb
@@ -1,102 +1,116 @@
 #https://www.bloc.io/ruby-warrior/#/warriors/10993/levels/9
 
 #ugly code
-class Player
-  def initialize
-    super
-  end
-  
-  def is_not_being_attacked? warrior
-    warrior.health >= @health
-  end
-  
-  def is_being_attacked! warrior
-    if is_not_being_attacked? warrior
-      warrior.rest!
-    else
-      if warrior.health < 15
-          #defensive retrea
-          #TODO detect attack direction
+  class Player		
+		def being_attacked? warrior
+			warrior.health < @health
+		end
+	 
+		def retract! warrior
+			if being_attacked? warrior and warrior.health < 12
+				#defensive retrea
+        spaces_back = warrior.look(:backward)
+				spaces_front = warrior.look(:forward)
+				if spaces_back.any? {|s| s.enemy?}
+					warrior.walk!(:forward)
+					return true
+				elsif spaces_front.any? {|s| s.enemy?}
           warrior.walk!(:backward)
-      else
-          #in a flight attack anyway
-          warrior.walk!
-      end
-    end
-  end
-  
-  def may_retract warrior
-    if warrior.health < 15
-      spaces_back = warrior.look(:backward)
-      spaces_front = warrior.look(:forward)
-      
-      if spaces_back.any? {|s| s.enemy?}
-        warrior.shoot!(:backward)
-      elsif spaces_front.any? {|s| s.enemy?}
-        warrior.shoot!(:forward)
-      else
-        warrior.walk!(:backward)
-      end
-      
-    else
-      warrior.rest!
-    end
-  end
-  
-  def may_use_sword space,warrior
-      if space.empty?
-        if warrior.health < 20
-          is_being_attacked! warrior
-        else
-          warrior.walk!
+          return true
         end
-      else
-          if space.enemy?
-              warrior.attack!
-              @health = warrior.health
-              return true
+			else
+				false
+			end
+		end
+
+		def rest! warrior
+				if warrior.health < 20 and not reach_staris? warrior and not being_attacked? warrior and warrior.health < 12
+					warrior.rest!
+					true
+				else
+					false
+				end
+		end
+
+		def reach_staris? warrior
+				spaces = warrior.look
+				if not spaces.any? {|s| s.enemy?} and spaces[0].empty? and spaces[-1].stairs?
+					true
+				else
+					false
+				end
+		end
+
+		def attack! warrior
+			spaces = warrior.look
+			if spaces[0].empty?
+				spaces_back = warrior.look(:backward)
+				spaces_front = warrior.look(:forward)
+				
+				if spaces_back.any? {|s| s.enemy?}
+					unless spaces_back.any? {|s| s.captive?}
+            warrior.shoot!(:backward)
+					  return true
           end
-          if space.captive?
-              warrior.rescue!
-              @health = warrior.health
-              return true
+				elsif spaces_front.any? {|s| s.enemy?}
+          unless spaces_front.any? {|s| s.captive?}
+					  warrior.shoot!(:forward)
+					  return true
           end
-      end
-  end
-  
-  def think_act(warrior,space)
-    spaces = warrior.look
-    if not spaces.any? {|s| s.captive?} and spaces.any? {|s| s.enemy?} and space.empty?
-        unless is_not_being_attacked? warrior
-          unless may_use_sword space,warrior
-            warrior.shoot!
-            @health = warrior.health
-          end
-        else
-          if warrior.health < 20
-            may_retract warrior
-            warrior
-          else
-            warrior.walk!
-            @health = warrior.health
-          end
+				else
+					false
+				end
+			else
+        if not spaces[0].captive? and not spaces[0].wall?
+				  warrior.attack!
+				  return true
         end
-    else
-      may_use_sword space,warrior
-    end
-  end
-  
-  def play_turn(warrior)
-    if @health.nil?
-      @health = warrior.health
-    end
-    
-    space = warrior.feel
-    if space.wall?
-      warrior.pivot!
-    else
-      think_act warrior,space
-    end
-    @health = warrior.health
-  end
+			end
+		end
+
+
+		def walk! warrior
+			#assume not need to retract
+			space = warrior.feel
+
+      if space.wall?
+        warrior.pivot!
+        true
+			elsif space.empty?
+
+				warrior.walk!
+				return true
+        
+			else
+				false
+			end
+		end
+		
+		def rescue! warrior
+			space = warrior.feel
+				if space.captive?
+					warrior.rescue!
+					return true
+				else
+					false
+				end
+		end
+
+		def play_turn(warrior)
+
+			@health ||= warrior.health
+			# ugly trick can be elgant solution if used well
+			#$warrior = warrior
+		 
+			if retract! warrior
+			elsif rest! warrior
+			elsif attack! warrior
+			elsif walk! warrior
+			elsif rescue! warrior
+			else
+				puts "Something wrong."
+			end
+
+			@health = warrior.health
+		end
 end
